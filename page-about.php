15a89ad225a2513919033836c67593ac2ebba921
shida <c.shida4223@gmail.com>
2026-02-28 11:19:57 +0900

    abouページリニューアル

--- page-about.php
+++ page-about.php
@@ -199,167 +199,78 @@
           </div>
         </section>
         <section>
-          <div class="p-about-section p-about-sales">
-            <div class="p-heading">
-              <div class="p-heading__title">
-                <h2 class="c-title" data-ja="歯科技工営業"><span class="c-title--red">S</span>ales&nbsp;dental&nbsp;tech</h2>
-              </div>
-            </div>
-            <div class="p-about-section__content">
-              <div class="p-about-sales__images">
-                <div class="p-about-sales__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_sales_01.webp')); ?>">
+          <div class="p-about-section p-about-recruit">
+            <div class="p-recruit-cta">
+              <div class="p-recruit-cta__inner">
+                <p class="p-recruit-cta__eyebrow">For Those Who Share Our Values</p>
+                <h3 class="p-recruit-cta__title">私たちは、ともに働く仲間を募集しています。</h3>
+                <p class="p-recruit-cta__text">
+                  私たちは、歯科技工士・営業・事務など、さまざまな立場のメンバーが<br>
+                  ともに歯科技工に向き合う仲間を募集しています。
+                </p>
+                <div class="p-about-recruit__btn c-button">
+                  <a href="<?php echo esc_url(home_url('/recruit/')); ?>" class="p-home-recruit__link">
+                    <span class="c-button__pageLink">募集要項</span>
+                  </a>
                 </div>
-                <div class="p-about-sales__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_sales_02.webp')); ?>">
-                </div>
-              </div>
-              <div class="p-about-section__text">
-                <p>
-                  歯科技工製品の知識を持つお客様の担当営業が、ご要望に沿った提案とサポートをさせていただきます。
-                </p>
-                <p>
-                  営業社員は有限会社エフイスの所属となり事務所は当社施設内に併設されています。
-                </p>
-                <p>
-                  わたしたちは一緒に働く営業社員を募集しています。
-                </p>
-              </div>
-              <div class="p-about-recruit__btn c-button">
-                <a href="<?php echo esc_url(home_url('/recruit/')); ?>" class="p-home-recruit__link">
-                  <span class="c-button__pageLink">募集要項</span>
-                </a>
               </div>
             </div>
           </div>
         </section>
         <section>
-          <div class="p-about-section p-about-planning">
-            <div class="p-heading">
-              <div class="p-heading__title">
-                <h2 class="c-title" data-ja="歯科技工販売企画"><span class="c-title--red">S</span>ales&nbsp;Planning</h2>
+          <div class="p-about-section">
+            <div class="p-company-outline" id="outline">
+              <div class="p-company-outline__title text-center">
+                <h2 class="c-title" data-ja="会社概要"><span class="c-title--red">Outline</span></h2>
               </div>
-            </div>
-            <div class="p-about-section__content">
-              <div class="p-about-planning__images">
-                <div class="p-about-planning__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_planning_01.webp')); ?>">
-                </div>
-                <div class="p-about-planning__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_planning_02.webp')); ?>">
-                </div>
-                <div class="p-about-planning__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_planning_03.webp')); ?>">
-                </div>
-              </div>
-              <div class="p-about-section__text">
-                <p>
-                  最新の材料、機器、製作方法の得た情報を元に歯科技工製品カタログ、簡便なパッキング、指示書などを企画立案、制作します。
-                </p>
-                <p>
-                  営業社員は有限会社エフイスの所属となり事務所は当社施設内に併設されています。
-                </p>
-                <p>
-                  わたしたちは一緒に働く事務社員を募集しています。
-                </p>
-              </div>
-              <div class="p-about-recruit__btn c-button">
-                <a href="<?php echo esc_url(home_url('/recruit/')); ?>" class="p-home-recruit__link">
-                  <span class="c-button__pageLink">募集要項</span>
-                </a>
+              <div class="p-page-section__content">
+                <?php the_content(); ?>
               </div>
             </div>
           </div>
         </section>
         <section>
-          <div class="p-about-section p-about-communication">
-            <div class="p-heading">
-              <div class="p-heading__title">
-                <h2 class="c-title" data-ja="コミュニケーション"><span class="c-title--red">C</span>ommunication</h2>
+          <div class="p-about-section">
+            <div class="p-company-history" id="history">
+              <div class="p-company-history__title text-center">
+                <h2 class="c-title" data-ja="沿 革"><span class="c-title--red">History</span></h2>
               </div>
-            </div>
-            <div class="p-about-section__content">
-              <div class="p-about-communication__images">
-                <div class="p-about-communication__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_communication_01.webp')); ?>">
+              <div class="p-page-section__content">
+                <div class="p-company-history__list">
+                  <dl class="p-company-history__item">
+                    <dt><span>1981</span>年</dt>
+                    <dd>東京大森に移転/イタリア製技工デスクユニット「デンタルアート」を輸入し設置</dd>
+                  </dl>
+                  <dl class="p-company-history__item">
+                    <dt><span>1984</span>年</dt>
+                    <dd>神奈川県川崎市に移転「ベルデンタルラボラトリー株式会社」設立</dd>
+                  </dl>
+                  <dl class="p-company-history__item">
+                    <dt><span>1987</span>年</dt>
+                    <dd>東京大森に移転/イタリア製技工デスクユニット「デンタルアート」を輸入し設置</dd>
+                  </dl>
+                  <dl class="p-company-history__item">
+                    <dt><span>1994</span>年</dt>
+                    <dd>東京蒲田に移転</dd>
+                  </dl>
+                  <dl class="p-company-history__item">
+                    <dt><span>2003</span>年</dt>
+                    <dd>営業所を分社化し「有限会社エフイス」設立/プロセラシステム(ノーベルバイオケア社)導</dd>
+                  </dl>
+                  <dl class="p-company-history__item">
+                    <dt><span>2012</span>年</dt>
+                    <dd>本社ビルの大規模改築により機能を充実化/CADCAM機器の設置を開始</dd>
+                  </dl>
+                  <dl class="p-company-history__item">
+                    <dt><span>2023</span>年</dt>
+                    <dd>東京西六郷に新社屋を竣工し移転</dd>
+                  </dl>
                 </div>
-                <div class="p-about-communication__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_communication_02.webp')); ?>">
-                </div>
-                <div class="p-about-communication__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_communication_03.webp')); ?>">
-                </div>
-              </div>
-              <div class="p-about-section__text">
-                <p>
-                  小さな会社ですがわたくしたちは、”社員は家族”をビジョンとしている会社です。<br>W65.（BDLとFIS）ファミリーの一員に加わり、歯科技工士、歯科技工営業、歯科技工販売企画として、素晴らしいキャリアを築くチャンスを見つけましょう。
-                </p>
-                <p>
-                  当社W65.（BDLとFIS）で求人募集を行っています。<br>単なる家族経営の企業ではなく、社員間コミュニケーションの良い会社です。
-                </p>
-                <p>
-                  当社W65.（BDLとFIS）では、多様なバックグラウンドを持つ才能ある方々に、やりがいのある歯科技工士のキャリアを提供しています。<br>競争力のある給与、充実した福利厚生、そしてキャリアアップをサポートする研修機会で人生をお楽しみください。
-                </p>
-                <p>
-                  社内、ラウンジ、テラスなど、ワークライフバランスへの取り組みも充実しており、仕事と仲間との絆を育むことができます
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div class="p-about-section p-about-health">
-            <div class="p-heading">
-              <div class="p-heading__title">
-                <h2 class="c-title" data-ja="健康診断"><span class="c-title--red">H</span>ealth&nbsp;check</h2>
-              </div>
-            </div>
-            <div class="p-about-section__content">
-              <div class="p-about-health__images">
-                <div class="p-about-health__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_health_01.webp')); ?>">
-                </div>
-                <div class="p-about-health__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_health_02.webp')); ?>">
-                </div>
-                <div class="p-about-health__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_health_03.webp')); ?>">
-                </div>
-              </div>
-              <div class="p-about-section__text">
-                <p>
-                  三階の多目的ラウンジをさながらクリニックに、歯科技工士、歯科技工営業、歯科技工販売企画の全社員が毎年、健康診断を受診します。
-                </p>
               </div>
             </div>
           </div>
         </section>
-        <section>
-          <div class="p-about-section p-about-environme">
-            <div class="p-heading">
-              <div class="p-heading__title">
-                <h2 class="c-title" data-ja="作業環境測定"><span class="c-title--red">W</span>ork&nbsp;environme</h2>
-              </div>
-            </div>
-            <div class="p-about-section__content">
-              <div class="p-about-environme__images">
-                <div class="p-about-environme__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_environme_01.webp')); ?>">
-                </div>
-                <div class="p-about-environme__img">
-                  <img src="<?php echo esc_url(get_theme_file_uri('assets/images/about_environme_02.webp')); ?>">
-                </div>
-              </div>
-              <div class="p-about-section__text">
-                <p>
-                  歯科技工室は労働安全法に基づく作業環境測定を毎年行い、歯科技工士の健康に悪影響がないよう努めています。
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-
-
+        
       </div>
     </div>
   </div>
