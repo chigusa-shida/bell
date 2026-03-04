7ad87135eacf188ebae66b282074f1ea44ab66c0
shida <c.shida4223@gmail.com>
2026-03-04 15:41:40 +0900

    お取引についてページ作成

---
+++ page-transaction.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Template Name: お取引について
+*/
+get_header();
+?>
+<main>
+    <div class="l-main" id="js-main">
+        <div class="p-transaction">
+            <div class="p-heading">
+                <div class="p-heading__content">
+                    <div class="p-heading__text">
+                        <h1 class="p-heading__lead c-lead">お取引について</h1>
+                        <div class="p-heading__detail text-center">
+                            <p>
+                                ベルデンタルラボラボラトリーお選びいただき、誠にありがとうございます。
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+            <div class="p-page-section">
+                <div class="p-transaction__container">
+                    <div class="p-page-section__heading">
+                        <span class="c-title--red">Pich&nbsp;Up&nbsp;&&nbsp;Delivery</span>
+                        <h3 class="c-heading">訪問による受納品</h3>
+                    </div>
+                    <div class="p-transaction__content">
+                        <div class="p-transaction__text">
+                            <p>配達専門の社員、または技工班長または営業社員がお客様に訪問して受納品を行います。</p>
+                            <p>私たちは、作業効率を推進しています。</p>
+                            <p>社員の休暇や祝祭日、製作納期日程の都合により宅配便をご利用させていただく場合があります。</p>
+                            <p>ご理解のほどよろしくお願いいたします。</p>
+
+                            <div class="p-transaction__note">
+                                <p>
+                                    初めて弊社とお取引いただいた歯科医院様は、まずは下記へお電話くださいませ。<br>
+                                    03-6424-7829（ベルデンタルラボラトリー）<br>
+                                    03-6424-5224（エフイス）
+                                </p>
+                            </div>
+                        </div>
+                        <div class="p-transaction__media">
+                            <img class="p-transaction__img" src="<?php echo esc_url(get_theme_file_uri('assets/images/transaction_img_01.webp')); ?>" alt="">
+                        </div>
+                    </div>
+                    <div class="p-transaction__actions">
+                        <a class="c-button-red" href="#">技工指示書ダウンロード</a>
+                        <a class="c-button-red" href="#">納品書ダウンロード</a>
+                    </div>
+                </div>
+                <div class="p-transaction__container p-page-section">
+                    <div class="p-page-section__heading">
+                        <span class="c-title--red">Send&nbsp;Case</span>
+                        <h3 class="c-heading">宅配による受納品</h3>
+                    </div>
+                    <div class="p-transaction__content">
+                        <div class="p-transaction__text">
+                            <p>東京都下また近県および他府県の歯科医院様。<br>宅配便を利用した受納品で対応させていただきます</p>
+                            <p>始めて弊社をお選びいただいた歯科医院さまは、お電話いただくことでパッキングセットをお送りさせていただきます。<br>お急ぎの歯科医院様は下記のボタンより指示書と納期表をダウンロードし、必要事項をご記入うえ患者様の模型をお送りください。</p>
+
+                            <div class="p-transaction__note">
+                                <p>
+                                送り先
+                                <br>〒144-0056&nbsp;東京都大田区西六郷2-44-6&nbsp;W65.
+                                <br>ベルデンタルラボラトリー株式会社
+                                <br>有限会社エフイス
+                                </p>
+                            </div>
+                        </div>
+                        <div class="p-transaction__media">
+                            <img class="p-transaction__img p-transaction__img--right" src="<?php echo esc_url(get_theme_file_uri('assets/images/transaction_img_02_1.webp')); ?>" alt="">
+                            <img class="p-transaction__img p-transaction__img--left" src="<?php echo esc_url(get_theme_file_uri('assets/images/transaction_img_02_2.webp')); ?>" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="p-transaction__container">
+                    <div class="p-page-section__heading">
+                        <span class="c-title--red">ONLINE&nbsp;SHOP</span>
+                        <h3 class="c-heading">WEBによる受納品</h3>
+                    </div>
+                    <div class="p-transaction__content">
+                        <div class="p-transaction__text">
+                            <p>弊社独自の歯科技工製品ショッピングサイトです。</p>
+                            <p>歯科技工製品の選択ご指定から指示書作成まで簡単に手続きできます。</p>
+                            <p>社員の休暇や祝祭日、製作納期日程の都合により宅配便をご利用させていただく場合があります。</p>
+                            <p>先ずは会員登録をしていただきますと、パッキングセットをお送りいたします。</p>
+                        </div>
+                        <div class="p-transaction__media is-one">
+                            <img class="p-transaction__img" src="<?php echo esc_url(get_theme_file_uri('assets/images/dentalbin24_logo.webp')); ?>" alt="">
+                        </div>
+                    </div>
+                    <div class="p-transaction__actions">
+                         <a class="c-button-red" href="#">デンタル便24で注文する</a>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+</main>
+<?php get_template_part('template-parts/footer-top'); ?>
+<?php get_footer(); ?>
